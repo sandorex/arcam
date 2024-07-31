8d036e4107c75649d43e973161796981b4900bf9
sandorex <rzhw3h@gmail.com>
2024-07-31 23:40:05 +0000

    Make build.rs rerun each git change

--- build.rs
+++ build.rs
@@ -4,6 +4,8 @@ use std::process::Command;
 
 // basically just get commit hash if not tagged and add it as env var
 fn main() {
+    println!("cargo::rerun-if-changed=.git");
+
     let is_tagged: bool = match Command::new("git").args(&["tag", "--points-at", "HEAD"]).output() {
         Ok(output) => !String::from_utf8(output.stdout).unwrap().is_empty(),
         // assume not tagged even though git may not exist
