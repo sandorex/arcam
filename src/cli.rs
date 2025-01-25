e89a3bf6e2795a9da8a88f23b2376061c1b66c45
sandorex <rzhw3h@gmail.com>
2025-01-25 15:30:55 +0100

    WIP grouped up permission arguments in start command
    This has improved readability a lot!

--- src/cli.rs
+++ src/cli.rs
@@ -70,6 +70,8 @@ impl ConfigArg {
     }
 }
 
+const START_HEADING_PERMISSIONS: &str = "Permissions";
+
 #[derive(Args, Debug, Clone)]
 pub struct CmdStartArgs {
     /// Enter shell after container initialization finishes
@@ -82,57 +84,23 @@ pub struct CmdStartArgs {
     #[arg(long, env = crate::ENV_CONTAINER)]
     pub name: Option<String>,
 
-    /// Path to directory which will be used as /etc/skel inside the container
-    ///
-    /// Used for static dotfiles that can be copied verbatim
-    #[arg(long)]
-    pub skel: Option<String>,
-
     /// Set container default shell
     #[arg(long)]
     pub shell: Option<String>,
 
-    /// Set network access permission for the container
-    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1)]
-    pub network: Option<bool>,
-
-    /// Try to pass audio into the the container, security impact is unknown
-    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1)]
-    pub audio: Option<bool>,
-
-    /// Passes wayland compositor through, pokes holes in sandbox, allows r/w access to clipboard
+    /// Path to directory which will be used as /etc/skel inside the container
     ///
-    /// If you want to pass through a specific wayland socket use env var ARCAM_WAYLAND_DISPLAY
-    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1)]
-    pub wayland: Option<bool>,
-
-    /// Pass through ssh-agent socket
-    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1)]
-    pub ssh_agent: Option<bool>,
-
-    /// Pass through session dbus socket, allows command execution on host!
-    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1)]
-    pub session_bus: Option<bool>,
+    /// Used for static dotfiles that can be copied verbatim
+    #[arg(long, value_name = "DIR")]
+    pub skel: Option<String>,
 
     /// Run command on init, ran before all other scripts (ran using `/bin/sh`)
-    #[arg(long, value_name = "SCRIPT")]
+    #[arg(long, value_name = "COMMAND")]
     pub on_init_pre: Vec<String>,
 
     /// Run command on init, ran after all other scripts (ran using `/bin/sh`)
-    #[arg(long, value_name = "SCRIPT")]
+    #[arg(long, value_name = "COMMAND")]
     pub on_init_post: Vec<String>,
-
-    /// Pass through container port to host (both TCP and UDP)
-    ///
-    /// Not all ports are allowed with rootless podman
-    #[arg(short, long = "port", value_name = "PORT[:HOST_PORT]", value_parser = parse_ports)]
-    pub ports: Vec<(u32, u32)>,
-
-    /// Add capabilities, or drop them with by prefixing `!cap`
-    ///
-    /// For more details about capabilities read `man 7 capabilities`
-    #[arg(long = "cap", value_name = "[!]CAPABILITY")]
-    pub capabilities: Vec<String>,
 
     /// Mount additional paths inside workspace
     #[arg(short, long, value_name = "DIRECTORY")]
@@ -141,6 +109,40 @@ pub struct CmdStartArgs {
     /// Environment variables to set inside the container
     #[arg(short, long, value_name = "VAR=VALUE")]
     pub env: Vec<String>,
+
+    /// Set network access permission for the container
+    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1, help_heading = START_HEADING_PERMISSIONS)]
+    pub network: Option<bool>,
+
+    /// Try to pass audio into the the container, security impact is unknown
+    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1, help_heading = START_HEADING_PERMISSIONS)]
+    pub audio: Option<bool>,
+
+    /// Passes wayland compositor through, pokes holes in sandbox, allows r/w access to clipboard
+    ///
+    /// If you want to pass through a specific wayland socket use env var ARCAM_WAYLAND_DISPLAY
+    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1, help_heading = START_HEADING_PERMISSIONS)]
+    pub wayland: Option<bool>,
+
+    /// Pass through ssh-agent socket
+    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1, help_heading = START_HEADING_PERMISSIONS)]
+    pub ssh_agent: Option<bool>,
+
+    /// Pass through session dbus socket, allows command execution on host!
+    #[arg(long, value_name = "BOOL", default_missing_value = "true", require_equals = true, num_args = 0..=1, help_heading = START_HEADING_PERMISSIONS)]
+    pub session_bus: Option<bool>,
+
+    /// Pass through container port to host (both TCP and UDP)
+    ///
+    /// Not all ports are allowed with rootless podman
+    #[arg(short, long = "port", value_name = "PORT[:HOST_PORT]", value_parser = parse_ports, help_heading = START_HEADING_PERMISSIONS)]
+    pub ports: Vec<(u32, u32)>,
+
+    /// Add capabilities, or drop them with by prefixing `!cap`
+    ///
+    /// For more details about capabilities read `man 7 capabilities`
+    #[arg(long = "cap", value_name = "[!]CAPABILITY", help_heading = START_HEADING_PERMISSIONS)]
+    pub capabilities: Vec<String>,
 
     /// File, image or config to use to start a container
     #[arg(env = crate::ENV_IMAGE, value_parser = ConfigArg::parse, value_name = "FILE|IMAGE|@CONFIG")]
@@ -253,8 +255,8 @@ pub struct CmdKillArgs {
     #[arg(short, long)]
     pub yes: bool,
 
-    /// How many seconds to wait before killing the containers forcibly
-    #[arg(short, long, default_value_t = 20)]
+    /// Delay before container forcibly terminated (in seconds)
+    #[arg(short, long, default_value_t = 10)]
     pub timeout: u32,
 
     #[arg(value_name = "CONTAINER", default_value = "", env = crate::ENV_CONTAINER)]
@@ -274,31 +276,31 @@ pub struct CmdCompletionArgs {
 
 #[derive(Subcommand, Debug)]
 pub enum CliCommands {
-    /// Start a container in current directory, mounting it as the rw workspace
+    /// Start a container in current directory, mounting read-write
     Start(CmdStartArgs),
 
-    /// Enter the shell inside a running an owned container
+    /// Enter the shell inside a running container
     #[clap(visible_alias = "enter")]
     Shell(CmdShellArgs),
 
-    /// Execute a command inside a running an owned container
+    /// Execute a command inside a running container
     Exec(CmdExecArgs),
 
     /// Check if container exists
     ///
-    /// Exit code is 0 if container exists otherwise 1
+    /// Exit code is 0 if container is running otherwise 1
     Exists(CmdExistsArgs),
 
     /// Inspect a config, can be used as syntax check
     Config(CmdConfigArgs),
 
-    /// List running owned containers
+    /// List running containers
     List(CmdListArgs),
 
     /// Show container logs in journalctl
     Logs(CmdLogsArgs),
 
-    /// Stop running owned container
+    /// Stop running container
     #[clap(visible_alias = "stop")]
     Kill(CmdKillArgs),
 
