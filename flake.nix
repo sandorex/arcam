0e156b7963dc5cb5fb01e23f7c36c7a44b86c867
sandorex <rzhw3h@gmail.com>
2025-04-17 20:23:27 +0200

    chore: improve flake to pass commit to vergen during build

--- flake.nix
+++ flake.nix
@@ -2,21 +2,35 @@
   inputs = {
     naersk.url = "github:nix-community/naersk/master";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, utils, naersk }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk { };
-      in
-      {
-        packages.default = naersk-lib.buildPackage ./.;
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [ nano fish cargo rustc rustfmt rust-analyzer pre-commit rustPackages.clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-        };
-      }
-    );
+  outputs = { self, nixpkgs, naersk }:
+     let
+       inherit nixpkgs self;
+       package = system: (nixpkgs.legacyPackages.${system}.callPackage naersk {}).buildPackage {
+         src = ./.;
+
+         # make vergen_git2 happy
+         VERGEN_IDEMPOTENT = "1";
+         VERGEN_GIT_SHA = if (self ? "rev") then (builtins.substring 0 7 self.rev) else "nix-dirty";
+       };
+       shell = system: with import nixpkgs { inherit system; }; mkShell {
+            buildInputs = [ nano cargo rustc rustfmt rust-analyzer pre-commit rustPackages.clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+       };
+     in
+     {
+       packages.x86_64-linux.default = package "x86_64-linux";
+       packages.aarch64-linux.default = package "aarch64-linux";
+
+       devShells.x86_64-linux.default = shell "x86_64-linux";
+       devShells.aarch64-linux.default = shell "aarch64-linux";
+
+       # apple stuff (not officially supported)
+       packages.x86_64-darwin.default = package "x86_64-darwin";
+       packages.aarch64-darwin.default = package "aarch64-darwin";
+
+       devShells.x86_64-darwin.default = shell "x86_64-linux";
+       devShells.aarch64-darwin.default = shell "x86_64-linux";
+     };
 }
